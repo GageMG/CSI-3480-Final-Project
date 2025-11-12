3eb64e4d408fbeb048d66632a524e75c2be57a60
Gage Gjorgjeski <147656966+GageMG@users.noreply.github.com>
2025-11-12 01:13:44 -0500

    Lets see

--- backend/EndyPoint.py
+++ backend/EndyPoint.py
@@ -3,11 +3,11 @@ import requests
 #to define the API URL
 url = "THENWEPASTEITINHERE"
 
-#Going to write the GET 
+#Start the request 
 try:
     response = requests.get(url)
 
-    #to check successful response, I guess successful response is code 200
+    #check successful response, code 200
     if response.get() == 200:
         data = response.json()
         print("Data retrieved")
