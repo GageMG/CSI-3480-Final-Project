0a049666e2f7786f3ad5d91313f196e3c5b1feb2
GageMG <gagekace@gmail.com>
2025-11-17 16:56:31 -0500

    started functions.py

--- backend/API_Handling/Func.py
+++ backend/API_Handling/Func.py
@@ -1,19 +1,5 @@
-from flask import Flask, request, render_template
-from API import app
-import firebase_admin
-from firebase_admin import credentials, firestore
-from Init_fire import db
+from database import create_user, delete_user, get_salt, create_item, delete_item
 
-def reg_new_user(email, salt, verifier):
-    
-    try:
-        db.collection("users").document(email).set( {
-            "email": email,
-            "salt": salt,
-            "verifier": verifier
-        })
-        return True
-    except Exception as v:
-        print("Firestore error, ", v)
-        return False
+
+
     
