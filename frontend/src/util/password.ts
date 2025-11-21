96abbf93279e0a32e060b71c04e1df03c87027f8
Hunter Parcells <hunterparcells@gmail.com>
2025-11-20 23:06:09 -0500

    feat(backend): Use venv.

--- frontend/src/util/password.ts
+++ frontend/src/util/password.ts
@@ -2,12 +2,12 @@ const minimumPasswordLength = 8;
 const minimumPasswordNumberCount = 1;
 
 export function passwordMeetsRequirements(password: string) {
-  if(password.length < minimumPasswordLength) {
+  if (password.length < minimumPasswordLength) {
     return false;
   }
-  if((password.match(/\d/g) || []).length < minimumPasswordNumberCount) {
+  if ((password.match(/\d/g) ?? []).length < minimumPasswordNumberCount) {
     return false;
   }
-  
+
   return true;
 }
